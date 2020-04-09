9c3c789675d3048d4ae6cf4d9f73f498f4fb2d74
paltrickontpb <paltrickontpb@gmail.com>
2020-04-09 14:32:02 +0530

    Full Project

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const path = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  entry: './app/javascripts/app.js',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'app.js'
+  },
+  plugins: [
+    // Copy our app's index.html to the build folder.
+    new CopyWebpackPlugin([
+      { from: './app/index.html', to: "index.html" }
+    ]),
+    new CopyWebpackPlugin([
+      { from: './app/ballotmake.html', to: "ballotmake.html" }
+    ]),
+    new CopyWebpackPlugin([
+      { from: './app/results.html', to: "results.html" }
+    ]),
+    new CopyWebpackPlugin([
+      { from: './app/voting.html', to: "voting.html" }
+    ])
+  ],
+  module: {
+    rules: [
+      {
+       test: /\.css$/,
+       use: [ 'style-loader', 'css-loader' ]
+      }
+    ],
+    loaders: [
+      { test: /\.json$/, use: 'json-loader' },
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015'],
+          plugins: ['transform-runtime']
+        }
+      }
+    ]
+  }
+}
